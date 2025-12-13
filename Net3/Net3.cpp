42d523d2cf4d8fe721f7b8cdf956e7ed1e674748
Ivan Lukyanov <iukuanovivan@gmail.com>
2025-12-13 14:50:35 +0300

    Exercise 3

---
+++ Net3/Net3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+struct adress
+{
+    std::string town;
+    std::string name;
+    int home;
+    int flat;
+    int index;
+};
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+    adress n1 = { "Москва","Арбат",12,8,13456 };
+    adress n2 = { "Ижевск","Пушкина",59,143,953769 };
+    std::cout << "Город: " << n1.town << std::endl;
+    std::cout << "Улица: " << n1.name << std::endl;
+    std::cout << "Номер дома: " << n1.home << std::endl;
+    std::cout << "Номер квартиры: " << n1.flat << std::endl;
+    std::cout << "Индекс: " << n1.index << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Город: " << n2.town << std::endl;
+    std::cout << "Улица: " << n2.name << std::endl;
+    std::cout << "Номер дома: " << n2.home << std::endl;
+    std::cout << "Номер квартиры: " << n2.flat << std::endl;
+    std::cout << "Индекс: " << n2.index << std::endl;
+
+    return 0;
+}
